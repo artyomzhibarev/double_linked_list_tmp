938dcb832501b15e94ab932d699efd8fcd198476
artyomzhibarev <artyom.zhibarev@yandex.ru>
2021-02-16 11:40:30 +0300

    Fixes

--- double_linked_list_tmp.py
+++ double_linked_list_tmp.py
@@ -24,48 +24,9 @@ from typing import Any, Sequence, Optional
 
     Есть какой-то метод класса DoubleLinkedList хотите отработать в явном виде ещё раз, не возбраняется.
 """
-from typing import Any, Optional
 
 
-class Node:
-    """
-    Внутренний класс, класса LinkedList.
-    Пользователь напрямую не работает с узлами списка, узлами оперирует список.
-    """
-
-    def __init__(self, value: Any, next_: Optional['Node'] = None):
-        """
-        Создаем новый узел для односвязного списка
-        :param value: Любое значение, которое помещено в узел
-        :param next_: следующий узел, если он есть
-        """
-        self.value = value
-        self.next = next_  # Вызывается сеттер
-
-    @property
-    def next(self):
-        """Getter возвращает следующий узел связного списка"""
-        return self.__next
-
-    @next.setter
-    def next(self, next_: Optional['Node']):
-        """Setter проверяет и устанавливает следующий узел связного списка"""
-        self._check_node(next_)
-        self.__next = next_
-
-    def _check_node(self, node):
-        if not isinstance(node, self.__class__) and node is not None:
-            msg = f"Устанавливаемое значение должно быть экземпляром класса {self.__class__.__name__} " \
-                  f"или None, не {node.__class__.__name__}"
-            raise TypeError(msg)
-
-    def __repr__(self):
-        """Метод должен возвращать строку, показывающую, как может быть создан экземпляр."""
-        return f"Node({self.value}, next_={None})"
-
-    def __str__(self):
-        """Вызывается функциями str, print и format. Возвращает строковое представление объекта."""
-        return f"{self.value}"
+# ToDo импорт любой вашей реалиазации LinkedList
 
 
 class LinkedList:
@@ -94,11 +55,13 @@ class LinkedList:
         @next.setter
         def next(self, next_: Optional['Node']):
             """Setter проверяет и устанавливает следующий узел связного списка"""
-            if not isinstance(next_, self.__class__) and next_ is not None:
-                msg = f"Устанавливаемое значение должно быть экземпляром класса {self.__class__.__name__} " \
-                      f"или None, не {next_.__class__.__name__}"
-                raise TypeError(msg)
             self.__next = next_
+
+        def _check_node(self, node):
+            if not isinstance(node, self.__class__) and node is not None:
+                msg = f"Устанавливаемое значение должно быть экземпляром класса {self.__class__.__name__} " \
+                      f"или None, не {node.__class__.__name__}"
+                raise TypeError(msg)
 
         def __repr__(self):
             """Метод должен возвращать строку, показывающую, как может быть создан экземпляр."""
@@ -147,17 +110,42 @@ class LinkedList:
         return current_node
 
     def __getitem__(self, index: int) -> Any:
+        print(f'Method __getitem__ called')
         current_node = self.__step_by_step_on_nodes(index)
 
         return current_node.value
 
     def __setitem__(self, key, value: Any):
         current_node = self.__step_by_step_on_nodes(key)
-
         current_node.value = value
 
     def __reversed__(self):
         return iter(self[::-1])
+
+    def __value_iterator(self):
+        """
+        Generator
+        """
+        current_node = self.head
+        for _ in range(self.__len):
+            yield current_node
+            current_node = current_node.next
+            # if current_node.next.value is None:
+            #     raise StopIteration
+
+    def __iter__(self):
+        print(f'Method __iter__ called')
+        return self.__value_iterator()
+
+    # def __next__(self):
+    #     print(f'Method __next__ called')
+    #     if self.current_iter_node is None:
+    #         raise StopIteration
+    #
+    #     value = self.current_iter_node.value
+    #     self.current_iter_node = self.current_iter_node.next
+    #
+    #     return value
 
     def append(self, value: Any):
         """Добавление элемента в конец связного списка"""
@@ -244,7 +232,8 @@ class LinkedList:
                     flag = True
             iterations += 1
 
-    def is_iterable(self, data) -> bool:
+    @staticmethod
+    def is_iterable(data) -> bool:
         """Метод для проверки является ли объект итерируемым"""
         if hasattr(data, '__iter__'):
             return True
@@ -256,5 +245,51 @@ class LinkedList:
 
 
 class DoubleLinkedList(LinkedList):
-    class DoubleLinkedNode(Node):
+    class DoubleLinkedNode(LinkedList.Node):
+        def __init__(self, value: Any,
+                     next_: Optional['Node'] = None,
+                     prev: Optional['Node'] = None
+                     ):
+            # ToDo расширить возможности базового конструктора с учетом особенностей двусвязного списка
+            super().__init__(value, next_)
+            self.prev = prev
+
+        def __repr__(self) -> str:
+            """Метод должен возвращать строку, показывающую, как может быть создан экземпляр."""
+            # ToDo перегрузить метод
+            return f"{self.__class__.__name__}({self.value}, {self.next}, {self.prev})"
+
+        @property
+        def prev(self):
+            """Getter возвращает следующий узел связного списка"""
+            return self.__prev
+
+        @prev.setter
+        def prev(self, prev: Optional['Node']):
+            """Setter проверяет и устанавливает следующий узел связного списка"""
+            self._check_node(prev)
+            self.__prev = prev
+
+    def __init__(self, data: Sequence = None):
+        """Конструктор связного списка"""
+        super().__init__(data)
         ...
+
+    # - **`__str__`**
+    # - **`__repr__`**
+    # - **`__getitem__`**
+    # - **`__setitem__`**
+    # - **`__len__`**
+    # - **`insert`**
+    # - **`index`**
+    # - **`remove`**
+    # - **`append`**
+    # - **`__iter__`**
+
+
+if __name__ == '__main__':
+    ll = LinkedList([1, 2, 3, 4, 5, 6, 7])
+    dll = DoubleLinkedList([1, 2, 3, 4, 5, 6, 7])
+    print(dll)
+    print(ll)
+
